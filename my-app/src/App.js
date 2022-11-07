804ae07e54bd571db6b6d0e818d87fd7ac4957ab
André Licks <andrelicks@gmail.com>
2022-11-07 14:34:44 -0400

    Iniciando

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import icon1 from './Images/icon1.png'
+import icon2 from './Images/icon2.png'
+import icon3 from './Images/icon3.png'
+import icon4 from './Images/icon4.png'
+import ironhackLogo from './Images/ironhack-logo-xs.png'
+import menuTop from './Images/menu-top-xs.png'
+
+
+function App() {
+  return (
+    <div className="App">
+       </div>
+  );
+}
+
+export default App;
